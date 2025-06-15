e5fc85e112a435c613b5defdd6d58c58d3f8c48f
NicolasJulianCleber <julianneves12452@gmail.com>
2025-06-15 18:32:37 -0300

    sexto commit

--- App.js
+++ App.js
@@ -6,13 +6,13 @@ import {Modal, Modal2, Modal3} from './components/RecipeCard';
 const App = () => {
   // Exemplo de dados
   const data = [
-    {id: '1', nome: 'Bolinhos de chuva tradicionais:' },
+    {id: '1', nome: 'Bolinhos de chuva tradicionais: Os bolinhos de chuva são um clássico da culinária caseira, perfeitos para acompanhar um café da tarde ou nos dias frios. Feitos com uma massa simples à base de ovos, leite, açúcar e farinha, eles são fritos até ficarem douradinhos por fora e macios por dentro. Depois de prontos, são passados em uma mistura de açúcar e canela, ficando com aquele sabor de infância, aconchego e tradição.' },
   ];
   const data2 =[
-    {id: '1', nome: 'Bolo de chocolate fofinho:'},
+    {id: '1', nome: 'Bolo de chocolate fofinho: O bolo de chocolate fofinho é aquele tipo de receita simples e irresistível, perfeita para qualquer momento do dia. Feito com ingredientes básicos como ovos, leite, açúcar, farinha e bastante chocolate, ele fica com uma massa leve, úmida e cheia de sabor.'},
   ]
   const data3 =[
-    {id: '1', nome: 'Carne cozida com batatas:'},
+    {id: '1', nome: 'Carne cozida com batatas: A Carne Cozida com Batata é um prato clássico da culinária caseira brasileira, conhecido pelo seu sabor marcante e pelo caldo encorpado que combina perfeitamente com arroz branco. A carne, cozida lentamente, fica macia e suculenta, absorvendo os temperos do refogado de alho, cebola e tomate. As batatas, cozidas junto ao molho, ganham sabor e textura, servindo como um acompanhamento perfeito dentro do próprio prato.'},
   ]
   // Como renderizar cada item
   const renderItem = ({ item }) => (
@@ -38,6 +38,7 @@ const App = () => {
       <>
       <TouchableOpacity style={styles.button} onPress={() =>setModal(true)}>
         <Text>Bolinhos de chuva tradicionais</Text>
+  
       </TouchableOpacity>
       <Text style={styles.title}>
       <FlatList data={data2} keyExtractor={(item) => item.id} renderItem={renderItem}></FlatList>
@@ -45,6 +46,7 @@ const App = () => {
 
       <TouchableOpacity style={styles.button} onPress={() =>setModal2(true)}>
         <Text>Bolo de chocolate fofinho</Text>
+        
       </TouchableOpacity>
       <Text style={styles.title}>
         <FlatList data={data3} keyExtractor={(item) => item.id} renderItem={renderItem}></FlatList>
